bcfff86390dab33477354bf7bf5bf3c5dc0b31b0
Abhay S. Babu <abhays.babu88@gmail.com>
2025-07-24 13:15:49 +0530

    added typing...

--- socket/socket.js
+++ socket/socket.js
@@ -1,27 +1,43 @@
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 import http from 'http'
 import express from 'express'
 
-const app=express()
-const onlineUsers={}
-const server=http.createServer(app)
+const app = express()
+const onlineUsers = {}
+const server = http.createServer(app)
 
-const io=new Server(server,{
-    cors:{
-        origin:'*',
-        methods:['GET','POST'],
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
     }
 })
 
-export const GetReceiverSocketId=(receiverId)=>{
+export const GetReceiverSocketId = (receiverId) => {
     return onlineUsers[receiverId]
 }
-io.on('connection',(socket)=>{
-    console.log("user jointed",socket.id)
-    socket.on('join',(receiverId)=>{
-        onlineUsers[receiverId]=socket.id
-        console.log("Receiver id ",receiverId," socket id ", socket.id)
+io.on('connection', (socket) => {
+    console.log("user jointed", socket.id)
+    socket.on('join', (receiverId) => {
+        onlineUsers[receiverId] = socket.id
+        console.log("Receiver id ", receiverId, " socket id ", socket.id)
     })
+
+    socket.on('typing', ({ senderId, receiverId }) => {
+        const receiverSocketId = onlineUsers[receiverId];
+        console.log(receiverSocketId)
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit('userTyping', { senderId });
+        }
+    });
+
+    socket.on('stopTyping', ({ senderId, receiverId }) => {
+        const receiverSocketId = onlineUsers[receiverId];
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit('userStopTyping', { senderId });
+        }
+    });
+
 })
 
-export {app,server,io}
+export { app, server, io }
